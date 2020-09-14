dc5f4c1314a3d4e2627e9215ad766743a396a4d5
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-09-14 22:27:03 +0530

    Initial commit

---
+++ src/main/java/com/vijaygenius123/reddit_backend/RedditBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.reddit_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedditBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vijaygenius123/reddit_backend/RedditBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.reddit_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
